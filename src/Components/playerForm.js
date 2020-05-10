08a65a4a2c94772af544dce9548444b330bcfec3
rishichalla67 <rishichalla@ufl.edu>
2020-05-10 13:09:14 -0400

    Form Validation Complete

--- src/Components/playerForm.js
+++ src/Components/playerForm.js
@@ -4,25 +4,25 @@ import { TextField, Button} from "@material-ui/core";
 import Alert from '@material-ui/lab/Alert';
 import Menu from './Menu'
 import axios from 'axios'
+import * as yup from 'yup';
 
-function validateEmail(value) {
-    let error;
-    if (!value) {
-        error = 'Required';
-    } else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(value)) {
-        error = 'Invalid email address';
-    }
-    return error;
-}
 
 const MyTextField = ({placeholder, ...props}) => {
     const [field, meta] = useField(props);
     const errorText = meta.error && meta.touched ? meta.error : '';
 
     return(
-        <TextField {...field} placeholder={placeholder} helperText={errorText}/>
+        <TextField {...field} placeholder={placeholder} helperText={errorText} error={!!errorText}/>
     );
 };
+
+const validationSchema = yup.object({
+    First: yup.string().required().min(1),
+    Last: yup.string().required().min(1),
+    Email: yup.string().required().email()
+})
+
+
 
 export default function playerForm() {
 
@@ -52,19 +52,23 @@ export default function playerForm() {
                         Phone: '',
                         Handicap: ''
                     }}
-                    validate={(values) => {
-                        const errors = {};
-
-                        if(!(values.Email.includes('@')))
-                        {
-                            errors.Email = 'Please Enter a Valid Email'
-                        }
-                        return errors;
-                    }}
+                    validationSchema={validationSchema}
+                    // validate={(values) => {
+                    //     const errors = {};
+                    //
+                    //     if(!(values.First.includes(''))) {
+                    //         errors.First = 'Please Enter a First Name'
+                    //     }
+                    //     if(!(values.Last.includes(''))) {
+                    //         errors.Last = 'Please Enter a Last Name'
+                    //     }
+                    //     if(!(values.Email.includes('@'))) {
+                    //         errors.Email = 'Please Enter a Valid Email'
+                    //     }
+                    //
+                    //     return errors;
+                    // }}
                     onSubmit={(values, {setSubmitting}) => {
-                        if(values.First === ''){values.First = 'N/A'}
-                        if(values.Last === ''){values.Last = 'N/A'}
-                        if(values.Email === ''){values.Email = 'N/A'}
                         if(values.Phone === ''){values.Phone = 'N/A'}
                         if(values.Handicap === ''){values.Handicap = 'N/A'}
                         handleSubmit(values);
@@ -75,13 +79,12 @@ export default function playerForm() {
                     {({values, isSubmitting}) => (
                         <Form>
 
-                            <Field placeholder='First' name="First" type='input' as={TextField}/>
+                            <MyTextField placeholder='First' name="First" type='input' as={TextField}/>
                             <div>
-                                <Field placeholder='Last' name="Last" type='input' as={TextField}/>
+                                <MyTextField placeholder='Last' name="Last" type='input' as={TextField}/>
                             </div>
                             <div>
                                 <MyTextField placeholder='Email' name="Email" type='input' as={TextField}/>
-
                             </div>
                             <div>
                                 <Field placeholder='Phone' name="Phone" type='input' as={TextField}/>
