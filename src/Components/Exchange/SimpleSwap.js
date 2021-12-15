a3159d3a292ab0d2fea087e3c2eb31575db670e3
rchalla67 <rishichalla67@gmail.com>
2021-12-15 15:25:05 -0500

    removed exchange for mobile users since no one was able to use

--- src/Components/Exchange/SimpleSwap.js
+++ src/Components/Exchange/SimpleSwap.js
@@ -12,10 +12,18 @@ export default function SimpleSwap() {
     const history = useHistory()
 
     return (
-        <div className="bg">
-            <Menu/>
-            <h1 className="disclaimer">Accountless Crypto Swapping Powered By SimpleSwap</h1>
-            <iframe className="simpleSwap" id="simpleswap-frame" name="SimpleSwap Widget"  src="https://simpleswap.io/widget/9cc3620c-de2c-4c9e-b7b0-e49f350043bb" frameBorder="0"></iframe>
-        </div>
+        <>
+            <div className="bg not-mobile">
+                <Menu/>
+                <h1 className="disclaimer">Accountless Crypto Swapping Powered By SimpleSwap</h1>
+                <iframe className="simpleSwap" id="simpleswap-frame" name="SimpleSwap Widget"  src="https://simpleswap.io/widget/9cc3620c-de2c-4c9e-b7b0-e49f350043bb" frameBorder="0"></iframe>
+            </div>
+
+            <div className="bg mobile">
+                <Menu/>
+                <h2 className="disclaimer">Unfortunately this page isnt currently available for mobile users</h2>
+                <h3 className="sarcastic-redirect">Maybe someone in <Link to="/global-chat">chat</Link> will listen to you complain about it.</h3>
+            </div>
+        </>
     )
 }
